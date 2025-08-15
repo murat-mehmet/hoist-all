09a7045bca03575e320ad7417c6d78eb326ce85f
Murat Mehmet <info.muratmehmet@gmail.com>
2025-08-15 11:33:12 +0300

    add hoisting selected app

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,30 @@
 #!/usr/bin/env node
-const hoistAll = require("../lib/copyAppNodeModules");
 
-if (require.main === module) {
-  const [appsFolder, workspaceRoot] = process.argv.slice(2);
-  hoistAll(workspaceRoot || process.cwd(), appsFolder || 'apps');
-}
+// cli.js
+const { Command } = require('commander');
+const path = require('path');
+const hoistAll = require('../lib/hoist-all.js');
 
+const program = new Command();
+program.enablePositionalOptions();
+
+program
+  .name('hoist-all')
+  .description('Hoist or reverse-hoist node_modules between workspace root and apps')
+  .version('1.0.0');
+
+program
+  .argument('[app]', 'App to hoist')
+  .option('-r, --root <path>', 'Path to the workspace root', process.cwd())
+  .option('-a, --apps <path>', 'Folder containing your apps', 'apps')
+  .option('-v, --verbose', 'Enable verbose logging')
+  .action((app, options) => {
+    if (options.verbose) {
+      console.log(`Workspace root: ${path.resolve(options.root)}`);
+      console.log(`Apps folder: ${path.resolve(options.root, options.apps)}`);
+    }
+
+    hoistAll({workspaceRoot: options.root, appsFolder: options.apps, app: app});
+  });
+
+program.parse(process.argv);
